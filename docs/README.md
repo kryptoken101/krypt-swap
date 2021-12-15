1fc3f6b950225562000b123e3546ffafa46628f4
pearlium <87101200+kriscripting17@users.noreply.github.com>
2021-12-15 16:29:17 +0800

    Initial commit

---
+++ docs/README.md
@@ -0,0 +1,81 @@
+﻿[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](LICENSE)
+![Deploy to swaponline.github.io](https://github.com/swaponline/MultiCurrencyWallet/workflows/Deploy%20to%20swaponline.github.io/badge.svg)
+[![About SWAP token](https://img.shields.io/badge/ERC20-SWAP-green)](https://github.com/swaponline/MultiCurrencyWallet/blob/master/docs/SWAPTOKEN.md)
+![Custom badge](https://img.shields.io/endpoint?label=SWAP%20token%20price&url=https%3A%2F%2Fwallet.wpmix.net%2FtokenPrice%2F%3Fnetwork%3Dbsc%26address%3D0x92648e4537cdfa1ee743a244465a31aa034b1ce8%26round%3D2%26color%3Dgreen)
+   
+## MultiCurrencyWallet
+         
+- 👛 Crypto wallet: BTC, ETН (ERC20), BSC (BEP20), Matic (+tokens), USDT, {your_token}...
+- 💵 Fiat gateway: USD, EUR, RUB, UAH... via itez.com
+- ⚛️ P2P exchange – atomic swaps
+- 💡 Open-source, client-side
+- 📦 Embeddable into your site!
+- Marketmaker extention https://chrome.google.com/webstore/detail/multicurrencywallet/oldojieloelkkfeacfinhcngmbkepnlm
+- One-click WhiteLablel deply to AWS https://aws.amazon.com/marketplace/pp/prodview-jyrrj4txg5u3a
+
+Live demo here: 
+https://swaponline.io (mirror https://swaponline.github.io) . Testnet https://ropsten.swaponline.io/
+<br> Binance  Smart Chin : https://bsc.swap.io/ . Testnet: https://bsc-testnet.swaponline.site/
+
+
+No coding skills? Use :package: [WordPress plugin with admin panel](https://codecanyon.net/item/multicurrency-crypto-wallet-and-exchange-widgets-for-wordpress/23532064) :package: 
+
+Looking for investment opportunity? Read about [ERC20:SWAP token](/docs/SWAPTOKEN.md) (target price $1)
+
+### 1. Multi-currency wallet: your users can store Bitcoin and custom assets
+
+Add many assets to your wallet:
+
+<img src="https://wallet.wpmix.net/codecanyon_description_3.jpg">
+
+### 2. ERC20 token wallet
+
+<a href="https://mytoken.wpmix.net/">Wallet demo (custom asset "SWAP")</a>
+
+### 3. Secondary market (trading between users)
+
+<a href="https://swaponline.github.io/#/exchange/usdt-to-btc">Demo (orderbook)</a>
+
+
+## Easy to use in your project
+
+- Install [WordPress plugin](https://codecanyon.net/item/multicurrency-crypto-wallet-and-exchange-widgets-for-wordpress/23532064)
+- Or make your own build following the [installation guide](/docs/INSTALLATION.md)
+
+
+## Customizable (images, colors, texts...)
+
+- Using [WordPress plugin](https://codecanyon.net/item/multicurrency-crypto-wallet-and-exchange-widgets-for-wordpress/23532064)? Сustomize right in the admin panel!
+- Using your own build? Follow the [customization guide](/docs/CUSTOMIZATION.md)
+
+
+## Open for integrations
+
+See the [list of our clients](/docs/CLIENTS.md)
+
+
+## International
+
+Supported languages:
+
+- 🇬🇧 EN
+- 🇷🇺 RU
+- 🇳🇱 NL
+- 🇪🇸 ES
+- 🇵🇱 PL
+
+Work in progress to add more languages.
+
+
+## How to support the project?
+
+- Buy [SWAP token](/docs/SWAPTOKEN.md)
+- [Donate](/docs/DONATE.md)
+- _(the easiest)_ Star the project ⭐
+
+Developer? Start with [ONBOARDING](https://github.com/swaponline/MultiCurrencyWallet/blob/master/docs/ONBOARDING.md)
+
+---
+
+Any questions [are welcome](https://t.me/swaponline)
+
---
+++ src/front/shared/helpers/links.ts
@@ -0,0 +1,167 @@
+const linksManager = {
+  home: '/',
+  hashHome: '#/',
+
+  exchange: '/exchange',
+  quickSwap: '/exchange/quick',
+
+  // Market maker page - for run parallel swaps in background
+  marketmaker: '/marketmaker',
+  marketmaker_short: '/mm',
+
+  history: '/history',
+  createWallet: '/createWallet',
+  restoreWallet: '/restoreWallet',
+  connectWallet: '/connectWallet',
+  invoices: '/invoices',
+  invoice: '/invoice',
+  farm: 'https://farm.wpmix.net/',
+  localStorage: '/localStorage',
+
+  wallets: '/wallets',
+  send: '/withdraw',
+  savePrivateSeed: '/savePrivateSeed',
+  savePrivateKeys: '/savePrivateKeys',
+  currencyWallet: '/wallet',
+  atomicSwap: '/atomic-swaps',
+  turboSwap: '/turbo-swaps',
+  feed: '/feed',
+  test: 'https://testnet.swaponline.io',
+  main: 'https://swaponline.io/',
+  wallet: '/wallet',
+  notFound: '/NotFound',
+  multisign: '/multisign',
+  createInvoice: '/createinvoice',
+
+  // social networks
+  medium: '#',
+  twitter: 'https://twitter.com/SwapOnlineTeam',
+  facebook: '#',
+  github: 'https://github.com/swaponline/MultiCurrencyWallet',
+  telegram: 'https://t.me/swaponline',
+  bitcointalk: '#',
+  discord: '#',
+  reddit: '#',
+  youtube: '#',
+
+  transak: 'https://global.transak.com/',
+  transakDev: 'https://staging-global.transak.com/',
+  transactionRate: 'https://en.bitcoin.it/wiki/Maximum_transaction_rate#:~:text=Each%20transaction%20input%20requires%20at,the%20minimum-sized%20Bitcoin%20transaction',
+  impermanentLoss: 'https://academy.binance.com/en/articles/impermanent-loss-explained',
+  swapAudit: 'https://medium.com/swaponline/technical-audit-of-swap-core-is-finished-successfully-65a67bc5f58a',
+  currencyCourses: 'https://noxon.wpmix.net/cursAll.php',
+
+  // footer links
+  etherdelta: 'https://etherdelta.com/#0x14a52cf6b4f68431bd5d9524e4fcd6f41ce4ade9-ETH',
+  button: 'https://wiki.swaponline.io/about-swap-online/#b2b',
+  about: 'https://wiki.swaponline.io/about-swap-online/',
+  extension: 'https://chrome.google.com/webstore/detail/swaponline/oldojieloelkkfeacfinhcngmbkepnlm',
+  career: 'https://wiki.swaponline.io/careers-swap-online/',
+  contacts: 'https://wiki.swaponline.io/contacts-swap-online/',
+  concept: 'https://wiki.swaponline.io/en.pdf',
+  description: 'https://docs.google.com/document/d/1MWrDR5pc3FB7AiWBO0GyBWBSci3VuDKNvknkPHtTzl8/edit#heading=h.tml4qc7dhie9',
+  research: 'https://docs.google.com/spreadsheets/d/1qWFLK2y8oMH5Gfam-iwqXPzLtwNabzp_EL6QFxjSBc0/edit?usp=sharing',
+  wiki: 'https://wiki.swaponline.io',
+  bitcointalkSendTx: 'https://bitcointalk.org/index.php?topic=1938621.0',
+  privacyPolicy: 'https://drive.google.com/file/d/1LdsCOfX_pOJAMqlL4g6DfUpZrGF5eRe9/view?usp=sharing',
+  privacyPolicyDoc: 'https://drive.google.com/file/d/0Bz2ZwZCmFtj_N29fQVk4bV9tUXRscjFEcUkxVkFXdF9tY0ow/view?usp=sharing',
+  terms: 'https://drive.google.com/file/d/0Bz2ZwZCmFtj_Nm9qSm0tUm9Ia1kwVGhWRlVlVXRJTGZtYW5N/view?usp=sharing',
+  legalOpinion: 'https://drive.google.com/file/d/0Bz2ZwZCmFtj_WlNkY0ZYN0ZpNUo2VFVEeW9rWEVoTlNja0VZ/view?usp=sharing',
+  licence: 'https://www.teatmik.ee/en/personlegal/14477421-Swap-Online-O%C3%9C',
+  faq: [
+    {
+      question: 'What is the price of SWAP token?',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-1',
+    },
+    {
+      question: 'I don\'t see the Order, although the others can see it (or it can be seen from a different browser)',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-2',
+    },
+    {
+      question: 'Where are my private keys stored?',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-3',
+    },
+    {
+      question: 'Is KYC required?',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-4',
+    },
+    {
+      question: 'What swap pairs are available?',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-5',
+    },
+    {
+      question: 'Is there fee for trade?',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-6',
+    },
+    {
+      question: 'How much gas is being spent for swap?',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-7',
+    },
+    {
+      question: 'Min sum to trade?',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-8',
+    },
+    {
+      question: 'My swap got stuck and my Bitcoin has already been sent to the swap. What should I do?',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-9',
+    },
+    {
+      question: 'In what language is this written, and where I can find source code?',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-10',
+    },
+    {
+      question: 'Swap.Online token',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-11',
+    },
+    {
+      question: 'What is Swap.Button?',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-12',
+    },
+    {
+      question: 'Advantages of using the exchanger based on Swap.Online DEP',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-13',
+    },
+    {
+      question: 'Request is declined. Why?',
+      link: 'https://wiki.swaponline.io/faq/#swap-faq-5738',
+      id: 'requestDeclimed',
+    },
+  ],
+  // footer new links
+  footer: {
+    exchange: 'https://swap.online/exchange',
+    wallet: 'https://swap.online',
+    widget: 'https://widget.swap.online/',
+    chromeextantion: 'https://chrome.google.com/webstore/detail/swaponline/oldojieloelkkfeacfinhcngmbkepnlm',
+    forstablecoin: 'https://wiki.swaponline.io/for_stablecoins/',
+    fordexses: 'https://wiki.swaponline.io/for_dexs/',
+    forblockchains: 'https://wiki.swaponline.io/for_blockchains/',
+    whitepaper: 'https://wiki.swaponline.io/en.pdf',
+    wiki: 'https://wiki.swaponline.io/',
+    github: 'https://github.com/swaponline',
+    about: 'https://wiki.swaponline.io/about-swap-online/',
+    agreements: 'https://drive.google.com/file/d/0Bz2ZwZCmFtj_Nm9qSm0tUm9Ia1kwVGhWRlVlVXRJTGZtYW5N/view?usp=sharing',
+    privacypolicy: 'https://drive.google.com/file/d/1LdsCOfX_pOJAMqlL4g6DfUpZrGF5eRe9/view?usp=sharing',
+    legal: 'https://drive.google.com/file/d/0Bz2ZwZCmFtj_WlNkY0ZYN0ZpNUo2VFVEeW9rWEVoTlNja0VZ/view?usp=sharing',
+    contacts: 'https://wiki.swaponline.io/contacts-swap-online/',
+    comparsion: 'https://docs.google.com/spreadsheets/d/1HVWjTKexfPjan_LFnCwMazEZTvcVtYl_3NWFKQ0L3ZM/edit?usp=drive_web&ouid=100012339553469619949',
+    bankdashboard: 'https://wiki.swaponline.io/crypto_banking/',
+    lnresearch: 'https://wiki.swaponline.io/lightninghack-berlin-june2018/',
+  },
+
+  getFaqLink(faqID) {
+    for (let i = 0; i < this.faq.length; i++) {
+      if (this.faq[i].id
+        && (this.faq[i].id === faqID)
+      ) {
+        return this.faq[i].link
+      }
+    }
+    return false
+  },
+}
+
+export const getFullOrigin = () => `${location.origin}${location.pathname}#`
+
+
+export default linksManager
